b1ac48d7710dae90830830f160fbc3e20d6dc580
Laurence <daceilo@gmail.com>
2012-01-09 21:59:27 -0700

    With Security Plugin installed and basics initialized

--- ContractManagement/grails-app/conf/Config.groovy
+++ ContractManagement/grails-app/conf/Config.groovy
@@ -91,3 +91,8 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+// Added by the Spring Security Core plugin:
+grails.plugins.springsecurity.userLookup.userDomainClassName = 'ca.shaw.contractmanagement.User'
+grails.plugins.springsecurity.userLookup.authorityJoinClassName = 'ca.shaw.contractmanagement.UserRole'
+grails.plugins.springsecurity.authority.className = 'ca.shaw.contractmanagement.Role'
---
+++ ContractManagement/grails-app/domain/ca/shaw/contractmanagement/Role.groovy
@@ -0,0 +1,14 @@
+package ca.shaw.contractmanagement
+
+class Role {
+
+	String authority
+
+	static mapping = {
+		cache true
+	}
+
+	static constraints = {
+		authority blank: false, unique: true
+	}
+}
